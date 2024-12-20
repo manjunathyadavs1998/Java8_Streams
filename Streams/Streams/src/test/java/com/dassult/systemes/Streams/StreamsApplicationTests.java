9ddde28b88f98001f3f2a0658313fabf87ac1f1e
MSA63 <manjunathyadavs1998@gmail.com>
2024-12-20 13:17:40 +0530

    Maven Project

---
+++ Streams/Streams/src/test/java/com/dassult/systemes/Streams/StreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dassult.systemes.Streams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
